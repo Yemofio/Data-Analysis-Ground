580928138df894e207e1b11412135a5a4aa0a0ee
Yemofio <95033608+Yemofio@users.noreply.github.com>
2023-04-05 10:30:09 +0000

    Create Basic-AI.py

---
+++ Basic-AI.py
@@ -0,0 +1,54 @@
+# Import required libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+# Load data from source
+data = pd.read_csv('data.csv')
+
+# Data preprocessing and cleaning
+# Drop rows with missing values
+data.dropna(inplace=True)
+
+# Convert date column to datetime format
+data['date'] = pd.to_datetime(data['date'])
+
+# Define a function to calculate average value by category
+def calculate_average_value_by_category(data, category_column, value_column):
+    return data.groupby(category_column)[value_column].mean()
+
+# Define a function to calculate total value by category and date
+def calculate_total_value_by_category_and_date(data, category_column, date_column, value_column):
+    return data.groupby([category_column, date_column])[value_column].sum().unstack()
+
+# Define a function to generate a line chart using plotly
+def generate_line_chart(data, x_column, y_column, color_column, title):
+    fig = px.line(data, x=x_column, y=y_column, color=color_column, title=title)
+    return fig
+
+# Define a function to generate a bar chart using plotly
+def generate_bar_chart(data, x_column, y_column, color_column, title):
+    fig = px.bar(data, x=x_column, y=y_column, color=color_column, title=title)
+    return fig
+
+# Define the Dash app layout
+app = dash.Dash(__name__)
+app.layout = html.Div(children=[
+    html.H1(children='Auto-generated reports'),
+
+    # Generate line chart
+    dcc.Graph(id='line_chart', figure=generate_line_chart(data, 'date', 'value', 'category', 'Total Value by Category and Date')),
+
+    # Generate bar chart
+    dcc.Graph(id='bar_chart', figure=generate_bar_chart(data, 'category', 'value', 'date', 'Average Value by Category'))
+])
+
+# Run the Dash app
+if __name__ == '__main__':
+    app.run_server(debug=True)
